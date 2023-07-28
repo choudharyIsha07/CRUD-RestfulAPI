1b120c7eb818d045bdc316a34b2f51ec97cf76f8
Isha choudhary <90838593+choudharyIsha07@users.noreply.github.com>
2023-07-29 00:08:58 +0530

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 
+//setting up the connection
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://stnw06:root@dombi1.pizwe7e.mongodb.net/Momta2"
-      // "mongodb+srv://root:root@momta.wf246r6.mongodb.net/Momta"  ye mene use kri thi to ignore krna
+      "mongodb+srv://stnw06:root@dombi1.pizwe7e.mongodb.net/Momta2" //use your URL instead of this to connect to your mongodb database.
     );
     console.log(`Connected to mongodb ${conn.connection.host}`.bgGreen);
   } catch (error) {
